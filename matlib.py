11d464a9c8dbfc60714ed1b23009d0b10205818c
sgmanohar <sgmanohar@users.noreply.github.com>
2022-06-25 21:34:37 +0100

    add errorbarplot

--- matlib.py
+++ matlib.py
@@ -99,6 +99,22 @@ def nancat(X, axis=0, pad_val=np.nan):
      Y=cat_pair(Y,i,axis=axis,pad_val=pad_val) # and concatenate 
   return Y 
 
-# test case
-nancat(  [np.array([1,2,3])[:,None].T,[np.array([4,5]),np.array([6])]], 
-          axis=1 )
+def errorBarPlot(Y,x=None, within_subject=True):
+    """plot mean and standard error along dimension 0.
+       Y [ subjects, x_levels, lines ]   = the data to plot
+       within_subject: if True, subtract subject means before calculating s.e.m.
+       x : x values corresponding to the columns (axis 1) of Y """
+    if len(Y.shape)<3:
+        Y=Y[:,:,None]   # ensure Y is at least 3D
+    if x is None:       # default X values are integers
+        x = np.arange(Y.shape[1])
+    mY = np.mean(Y,axis=0) # calculate mean of Y across subjects
+    if within_subject: # use within subject errors?
+        dY = Y-np.mean(Y,axis=(1,2))[:,None,None] # subtract subject mean
+    else: # use total error
+        dY = Y
+    sY = np.std(Y,axis=0) / np.sqrt(Y.shape[0]) # calculate standard error
+    hplot = plt.plot(x, mY) # draw lines
+    nlines = mY.shape[1]
+    for i in range(nlines): # draw error areas
+      plt.fill_between(x, mY[:,i]+sY[:,i], mY[:,i]-sY[:,i],alpha=0.3)
